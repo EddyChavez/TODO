a678135669ea059079994b52165281182f067502
a <a>
2019-03-12 12:41:47 -0600

    RECUPERAR CONTRASEÑA VISTA REDIRECCION

--- gestion-de-usuarios-y-permisos-front-end/src/app/components/password/request-reset/request-reset.component.ts
+++ gestion-de-usuarios-y-permisos-front-end/src/app/components/password/request-reset/request-reset.component.ts
@@ -3,9 +3,8 @@ import { JarwisService } from '../../../services/jarwis.service';
 import { SnotifyService } from 'ng-snotify';
 
 @Component({
-  selector: 'app-request-reset',
-  templateUrl: './request-reset.component.html',
-  styleUrls: ['./request-reset.component.css']
+  selector: 'app-dashboard',
+  templateUrl: './request-reset.component.html'
 })
 export class RequestResetComponent implements OnInit {
   public form = {
